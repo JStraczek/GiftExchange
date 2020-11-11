973de4426c6f48ace9e5cbc06f6c1954e1a95574
JStraczek <kubastr98@gmail.com>
2020-11-11 16:10:14 +0100

    fixed subject

--- script.py
+++ script.py
@@ -1,14 +1,15 @@
+# -*- coding: utf-8 -*-
 import smtplib, ssl
 import requests
 
-def get_receiver_list():
-    class Receiver:
+class Receiver:
         def __init__(self, details):
             self.name = details[0]
             self.surname = details[1]
             self.email = details[2]
             self.tip = details[3]
-        
+
+def get_receiver_list():
 
     #DOWNLOADING FROM SHEETS
     r = requests.get("https://api.apispreadsheets.com/data/3399/")
@@ -39,15 +40,16 @@ context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     server.login(sender_email, password)
+    # receivers=[Receiver(['jakub', 'straczek', 'kubastr98@gmail.com', 'costam'])]
     for person in receivers:
         receiver_email = person.email
-        message = f"""\ 
-        Subject: Prezentodawansko
+        message = f""" 
+        Prezentodawansko\n\n
 
-        
-        Czesc, to tylko testowy mail, wylosowana osobe dostaniesz kiedy indziej JD,
+        Czesc {person.name} {person.surname},
+        to tylko testowy mail, wylosowana osobe dostaniesz kiedy indziej JD,
         Oto podpowiedz jaka otrzyma mikolaj/mikolajowa w sprawie Twojego prezentu: {person.tip}
         Wesolych swiat :))))) 
-        """
-        # TODO: Send email here
+        """.encode('utf-8')
+
         server.sendmail(sender_email, receiver_email, message)
