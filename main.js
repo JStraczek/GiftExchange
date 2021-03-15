e95802ef25242374e913421ff0b5054e723812d7
Jakub Strączek <35780483+JStraczek@users.noreply.github.com>
2021-03-15 10:25:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function SubmitForm(){
-    if(document.getElementById("passwordInput").value == "INSERT HERE"){ // INSERT YOUR PASSWORD (IT'S NOT SAFE)
     $.ajax({
-        url:"INSERT HERE", // INSERT YOUR SPREADSHEETS API LINK FOR IT TO WORK
+        url:"INSERT HERE", // INSERT YOUR SPREADSHEETS API LINK
         type:'post',
         data:$("#userInputForm").serializeArray(),
         success: function(){
@@ -11,9 +10,4 @@ function SubmitForm(){
           alert("There was an error :(")
         }
     })
-  }
 };
-
-function CheckPassword(){
-
-}
